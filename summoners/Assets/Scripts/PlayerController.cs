4c5b79fbd6fdc6f9de16e01c31f465a3b6df2f52
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2022-12-04 12:55:25 +0100

    Add scene

--- summoners/Assets/Scripts/PlayerController.cs
+++ summoners/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed;
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+
     public Transform orientation;
     float horizontalInput;
     float verticalInput;
@@ -25,6 +34,18 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerInput();
+        SpeedControl();
+        //Ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        //Handle drag
+        if (grounded)
+        {
+            Debug.Log("Grounded");
+            rb.drag = groundDrag;
+        }
+        else
+            rb.drag = 0;
     }
     private void FixedUpdate()
     {
@@ -43,5 +64,16 @@ public class PlayerController : MonoBehaviour
 
         //Add force to player
         rb.AddForce(moveDirection.normalized * moveSpeed * 10);
+    }
+    //Limit players speed to moveSpeed variable
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
