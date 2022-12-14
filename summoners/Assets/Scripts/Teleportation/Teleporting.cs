514240d4f29b60bff1c349357efaf7c0327b8fdd
Aleksander Wojciechowski <olekwu1@gmail.com>
2022-12-14 22:21:37 +0100

    Teleportation machine working (y)

--- summoners/Assets/Scripts/Teleportation/Teleporting.cs
+++ summoners/Assets/Scripts/Teleportation/Teleporting.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Teleporting : MonoBehaviour
 {
     public GameObject player;
-
     [SerializeField] GameObject tp1;
     [SerializeField] GameObject tp2;
     [SerializeField] GameObject tp3;
-
+    
+    public int selectedLvl = 0;
 
     void OnTriggerEnter()
     {
-        player.transform.position = tp1.transform.position;
+        switch (selectedLvl)
+        {      
+            case 1:
+                player.transform.position = tp1.transform.position; 
+                break;
+            case 2:
+                player.transform.position = tp2.transform.position;
+                break;
+           case 3:
+                player.transform.position = tp3.transform.position;
+                break;
+
+        }
     }
 }
