2b15ad4fef940cee955ecb4d741fb8d98ef8ce74
Aleksander Wojciechowski <olekwu1@gmail.com>
2022-12-14 21:05:02 +0100

    Nearly done

--- summoners/Assets/Scripts/InteractionSystem/InteractionPromptUI.cs
+++ summoners/Assets/Scripts/InteractionSystem/InteractionPromptUI.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InteractionPromptUI : MonoBehaviour
 {
     private Camera _mainCam;
-
+    [SerializeField] private GameObject _uiPanel;
+    [SerializeField] private TextMeshProUGUI _promptText;
+    
     private void Start()
     {
         _mainCam = Camera.main;
+        _uiPanel.SetActive(false);
     }
 
     private void LateUpdate()
     {
         var rotation = _mainCam.transform.rotation;
         transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+
+    public bool isDisplayed = false;
+    public void SetUp(string promptText)
+    {
+        _promptText.text = promptText;
+        _uiPanel.SetActive(true);
+        isDisplayed = true;
+
+    }
+
+    public void Close()
+    {
+        isDisplayed = false;
+        _uiPanel.SetActive(false);
     }
 }
--- summoners/Assets/Scripts/InteractionSystem/Interactor.cs
+++ summoners/Assets/Scripts/InteractionSystem/Interactor.cs
@@ -8,9 +8,12 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private InteractionPromptUI _interactionPromptUI;
 
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
+
+    private IInteractable _interactable;
 
     private void Update()
     {
@@ -19,11 +22,18 @@ public class Interactor : MonoBehaviour
         //Check for interaction item
         if(_numFound > 0)
         {
-            var interactable = _colliders[0].GetComponent<IInteractable>();
+            _interactable = _colliders[0].GetComponent<IInteractable>();
 
-            if(interactable != null && Keyboard.current.eKey.wasPressedThisFrame)
+            if(_interactable != null)
             {
-                interactable.Interact(this);
+                if (!_interactionPromptUI.isDisplayed) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+
+                if (Keyboard.current.eKey.wasPressedThisFrame) _interactable.Interact(this);
+            }
+            else
+            {
+                if (_interactable != null) _interactable = null;
+                if (_interactionPromptUI.isDisplayed) _interactionPromptUI.Close();
             }
         }
 
