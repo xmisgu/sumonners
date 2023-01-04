8a429e85163ff2a759739a3d78ba54c73dff9000
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-01-04 22:12:04 +0100

    remove chuj test chujcomment

--- summoners/Assets/Scripts/PlayerController.cs
+++ summoners/Assets/Scripts/PlayerController.cs
@@ -29,7 +29,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
-    //chuj git test chuj
     // Update is called once per frame
     void Update()
     {
