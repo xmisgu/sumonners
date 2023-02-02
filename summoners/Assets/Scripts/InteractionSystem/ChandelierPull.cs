9eac383081e492853467bd15e7dbe72890e75fec
SoVerrr <patryk.rokiczan@gmail.com>
2023-02-02 13:22:52 +0100

    Chandelier

--- summoners/Assets/Scripts/InteractionSystem/ChandelierPull.cs
+++ summoners/Assets/Scripts/InteractionSystem/ChandelierPull.cs
@@ -7,11 +7,24 @@ public class ChandelierPull : MonoBehaviour, IInteractable
     [SerializeField] private string _prompt;
     public GameObject arm;
     public string InteractionPrompt => _prompt;
-
+    private float rotationSpeed = 50f;
+    private bool isInteracted = false;
+    private Vector3 finalRotation = new Vector3(0, 0, 45);
     public bool Interact(Interactor interactor)
     {
-        arm.transform.eulerAngles = new Vector3(0, 0, 45);
+
+        isInteracted = true;
         Debug.Log("Level completed");
         return true;
+    }
+
+    private void Update()
+    {
+        if (isInteracted)
+        {
+            if (arm.transform.eulerAngles.z < finalRotation.z)
+                arm.transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+
+        }
     }
 }
