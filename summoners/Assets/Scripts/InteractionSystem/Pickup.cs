2c90d73695f05cff21c02953f7da14274a19a85b
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-03-22 19:26:34 +0100

    pickup wip

--- summoners/Assets/Scripts/InteractionSystem/Pickup.cs
+++ summoners/Assets/Scripts/InteractionSystem/Pickup.cs
@@ -9,7 +9,8 @@ public class Pickup : MonoBehaviour, IInteractable
     private float pickupSpeed = 5f;
     private bool isInteracted = false;
     public GameObject player;
-
+    public Rigidbody body;
+    bool isPickedUp = false;
     public bool Interact(Interactor interactor)
     {
 
@@ -17,14 +18,33 @@ public class Pickup : MonoBehaviour, IInteractable
         Debug.Log("Pickup");
         return true;
     }
-
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.FreezeRotation;
+    }
     private void Update()
     {
-        Vector3 playerPositionOffset = player.transform.position;
+        Vector3 playerPositionOffset = player.transform.position + player.transform.forward / 5f;
         if (isInteracted)
         {
-            GetComponent<Collider>().enabled = false;
-            transform.position = Vector3.MoveTowards(transform.position, playerPositionOffset, pickupSpeed * Time.deltaTime);
+            transform.SetPositionAndRotation(Vector3.MoveTowards(transform.position, playerPositionOffset, pickupSpeed * Time.deltaTime), new Quaternion(0, 0, 0, 0));
+            if (transform.position == playerPositionOffset)
+            {
+                isInteracted = false;
+                isPickedUp = true;
+            }
+        }
+        if (isPickedUp)
+        {
+            if (Physics.BoxCast(playerPositionOffset, new Vector3(0.01f, 0.01f, 0.01f), Vector3.forward / 10f))
+                transform.SetPositionAndRotation(playerPositionOffset, new Quaternion(0, 0, 0, 0));
+            else
+            {
+                body.constraints = RigidbodyConstraints.FreezePosition;
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+
         }
     }
 }
