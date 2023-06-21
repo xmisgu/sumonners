ec2047a332e2072a0a34f1d161a56a535d19ed93
xMikuuu <m.polatynski9@gmail.com>
2023-06-21 22:33:37 +0200

    :skull: clockFace need some fixation

--- summoners/Assets/Scripts/InteractionSystem/FireplaceButton.cs
+++ summoners/Assets/Scripts/InteractionSystem/FireplaceButton.cs
@@ -10,6 +10,9 @@ public class FireplaceButton : MonoBehaviour, IInteractable
     private Vector3 targetPosition = new Vector3(4.35f, 1.663f, 3.079f);
     private bool isInteracted = false;
     private float speed = 0.5f;
+    private float rotationSpeed = 50f;
+    private Vector3 finalRotation = new Vector3(0, 0, -100);
+    [SerializeField] private GameObject ClockFace;
 
     public bool Interact(Interactor interactor)
     {
@@ -23,9 +26,17 @@ public class FireplaceButton : MonoBehaviour, IInteractable
     private void Update()
     {
         var step = speed * Time.deltaTime; // calculate distance to move
+        var step2 = speed * Time.deltaTime * 60;
         if (isInteracted)
         {
             transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, step);
+           // ClockFace.transform.Rotate(new Vector3(0, 0, -100) * Time.deltaTime);
+            if (ClockFace.transform.rotation.z < 0.65)
+                Debug.Log(ClockFace.transform.rotation.z);
+                ClockFace.transform.Rotate(new Vector3(0, 0, -(rotationSpeed * Time.deltaTime)));
+            // if (ClockFace.transform.eulerAngles.z > finalRotation.z)
+            //     ClockFace.transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+            // ClockFace.Rotate
         }
 
 
